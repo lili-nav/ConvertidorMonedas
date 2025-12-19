0d43a2b1b9897a6ff3a28b05832a066d22612ee5
Yosho2009 <alonsozaid294@gmail.com>
2025-12-19 09:35:23 -0600

    agregue pesos mxn a pesos clm

---
+++ peso_mxn-peso_clm.py
@@ -0,0 +1,8 @@
+def convertidor():
+    print("convertiendo pesos mexicanos en pesos colombianos")
+    pm = float(input("introduce la cantidad de pesos mexicanos: "))
+    valor_pc = 00.0047
+    pesos = pm * valor_pc
+    print(f"{pm} pesos colombinos son {pesos} pesos colombianos")
+
+()
