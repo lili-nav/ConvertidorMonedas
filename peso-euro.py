0efbc113cebfb6b5cf150049f0d14664acc9315d
Yosho2009 <alonsozaid294@gmail.com>
2025-12-19 09:20:34 -0600

    convertor de pesos a euros israel

---
+++ peso-euro.py
@@ -0,0 +1,5 @@
+print("convirtiendo pesos a euros")
+pesos = float(input("introduce la cantidad de pesos: "))
+valor_euro = 21.14
+euros = pesos * valor_euro
+print(f"{pesos} pesos son {euros} euros")
