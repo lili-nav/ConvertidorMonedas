a3f20631bdb32cacfe89368fad97c9dcabc1acb7
Yosho2009 <alonsozaid294@gmail.com>
2025-12-19 09:31:22 -0600

    convertidor de pesos colombiaos a pesos mexicanos gustavo

---
+++ Peso_clm-peso_mxn.py
@@ -0,0 +1,8 @@
+def convertidor():
+    print("convertiendo pesos colombianos en pesos mexicanos")
+    pc = float(input("introduce la cantidad de pesos colombianos: "))
+    valor_pc = 00.0047
+    pesos = pc * valor_pc
+    print(f"{pc} pesos colombinos son {pesos} pesos mexicanos")
+
+()
